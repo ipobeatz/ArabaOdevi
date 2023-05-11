377e3807b761bbc69715aeec22d8fcc9fb854c30
mrtckr008 <mrtckr008@gmail.com>
2023-05-11 16:53:06 +0300

    Proje dosyası ekle.

---
+++ ArabaOdevi/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace ArabaOdevi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.Yardım = new System.Windows.Forms.ToolStripSplitButton();
+            this.hakkındaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yardımToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Kaydet = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 39);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(431, 258);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Plaka";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Marka";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Model";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Yakıt";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Vites";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Açıklama";
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "yil";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.Yardım,
+            this.Kaydet,
+            this.toolStripButton4});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.toolStrip1.Size = new System.Drawing.Size(431, 39);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = global::ArabaOdevi.Properties.Resources.add_button_icon_vector_21679778;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(36, 36);
+            this.toolStripButton1.Text = "Kişi Ekle";
+            this.toolStripButton1.Click += new System.EventHandler(this.AddComand);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = global::ArabaOdevi.Properties.Resources._84380;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(36, 36);
+            this.toolStripButton2.Text = "Kişi düzenle";
+            this.toolStripButton2.Click += new System.EventHandler(this.EditCommand);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = global::ArabaOdevi.Properties.Resources._61182;
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(36, 36);
+            this.toolStripButton3.Text = "Kişi Sil";
+            this.toolStripButton3.Click += new System.EventHandler(this.DeleteCommand);
+            // 
+            // Yardım
+            // 
+            this.Yardım.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.Yardım.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Yardım.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hakkındaToolStripMenuItem,
+            this.yardımToolStripMenuItem});
+            this.Yardım.Image = global::ArabaOdevi.Properties.Resources.download;
+            this.Yardım.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Yardım.Name = "Yardım";
+            this.Yardım.Size = new System.Drawing.Size(48, 36);
+            this.Yardım.Text = "toolStripSplitButton1";
+            // 
+            // hakkındaToolStripMenuItem
+            // 
+            this.hakkındaToolStripMenuItem.Name = "hakkındaToolStripMenuItem";
+            this.hakkındaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.hakkındaToolStripMenuItem.Text = "hakkında";
+            this.hakkındaToolStripMenuItem.Click += new System.EventHandler(this.hakkındaToolStripMenuItem_Click);
+            // 
+            // yardımToolStripMenuItem
+            // 
+            this.yardımToolStripMenuItem.Name = "yardımToolStripMenuItem";
+            this.yardımToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.yardımToolStripMenuItem.Text = "yardım";
+            // 
+            // Kaydet
+            // 
+            this.Kaydet.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Kaydet.Image = global::ArabaOdevi.Properties.Resources.png_transparent_computer_icons_floppy_disk_user_interface_icon_design_save_button_angle_user_interface_design_rectangle;
+            this.Kaydet.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Kaydet.Name = "Kaydet";
+            this.Kaydet.Size = new System.Drawing.Size(36, 36);
+            this.Kaydet.Text = "toolStripSplitButton2";
+            this.Kaydet.Click += new System.EventHandler(this.SaveCommand);
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(36, 36);
+            this.toolStripButton4.Text = "Aç";
+            this.toolStripButton4.Click += new System.EventHandler(this.LoadCommand);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 297);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(431, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 319);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Arabalar";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripSplitButton Yardım;
+        private System.Windows.Forms.ToolStripMenuItem hakkındaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yardımToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ToolStripButton Kaydet;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+    }
+}
+
---
+++ ArabaOdevi/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace ArabaOdevi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            ShowList();
+        }
+        List<Araba> arabalar = new List<Araba>()
+        {
+        new Araba()
+        {
+            Plaka = "14AB25",
+            Marka = "fiat",
+            Model = "egea",
+            Yakıt = "1.5",
+            Renk = "beyaz",
+            Vites = "6 vites" ,
+            KasaTipi = "beyaz kasa",
+            Açıklama = "sahibinden az kullanılmış",
+            Date = new DateTime(21)
+},
+        new Araba()
+        {
+            Plaka = "24AC28",
+            Marka = "mercedes",
+            Model = "a80",
+            Yakıt = "1.8",
+            Renk = "siyah",
+            Vites = "7 vites" ,
+            KasaTipi = "siyah kasa",
+            Açıklama = "sahibinden az kullanılmış",
+            Date = new DateTime(24)
+                }
+    };
+
+        public void ShowList()
+        {
+            listView1.Items.Clear();
+            foreach (Araba araba in arabalar)
+            {
+                AddList(araba);
+            }
+        }
+
+        public void AddList(Araba araba)
+        {
+            ListViewItem item = new ListViewItem(new string[]
+                {
+                araba.Plaka,
+                araba.Marka,
+                araba.Model,
+                araba.Yakıt,
+                araba.Renk,
+                araba.Vites,
+                araba.KasaTipi,
+                araba.Açıklama,
+                araba.Date.ToShortDateString(),
+
+                });
+            item.Tag = araba;
+            listView1.Items.Add(item);
+
+        }
+
+        void EditAraba(ListViewItem pItem, Araba araba)
+        {
+            pItem.SubItems[0].Text = araba.Plaka;
+            pItem.SubItems[1].Text = araba.Marka;
+            pItem.SubItems[2].Text = araba.Model;
+            pItem.SubItems[3].Text = araba.Yakıt;
+            pItem.SubItems[4].Text = araba.Renk;
+            pItem.SubItems[5].Text = araba.Vites;
+            pItem.SubItems[6].Text = araba.KasaTipi;
+            pItem.SubItems[7].Text = araba.Açıklama;
+            pItem.SubItems[8].Text = araba.Date.ToShortDateString();
+
+            pItem.Tag = araba;
+        }
+        private void AddComand(object sender, EventArgs e)
+        {
+            FmAraba frm = new FmAraba()
+            {
+                Text = "kişi ekle",
+                StartPosition = FormStartPosition.CenterParent,
+                araba = new Araba()
+            };
+            if (frm.ShowDialog() == DialogResult.OK)
+            {
+                arabalar.Add(frm.araba);
+                AddList(frm.araba);
+            }
+
+
+        }
+
+        void EditCommand(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+                return;
+            ListViewItem pItem = listView1.SelectedItems[0];
+            Araba secili = pItem.Tag as Araba;
+
+
+            FmAraba frm = new FmAraba()
+            {
+
+                Text = "kişi düzenle",
+                StartPosition = FormStartPosition.CenterParent,
+                araba = Clone(secili),
+
+            };
+
+            if (frm.ShowDialog() == DialogResult.OK)
+            {
+                secili = frm.araba;
+                EditAraba(pItem, secili);
+
+            }
+        }
+        Araba Clone(Araba araba)
+
+        {
+            return new Araba()
+            {
+                id = araba.ID,
+                Plaka = araba.Plaka,
+                Marka = araba.Marka,
+                Model = araba.Model,
+                Yakıt = araba.Yakıt,
+                Renk = araba.Renk,
+                Vites = araba.Vites,
+                KasaTipi = araba.KasaTipi,
+                Açıklama = araba.Açıklama,
+                Date = araba.Date,
+
+
+            };
+
+
+        }
+        [Serializable]
+        public class Araba
+        {
+            
+            public string id;
+            [Browsable(false)]
+            public string ID
+            {
+                get
+                {
+                    if (id == null)
+                        id = Guid.NewGuid().ToString();
+                    return id;
+                }
+                set { id = value; }
+            }
+            [Category("Araba bilgileri"),DisplayName("Adı")]
+            public string Plaka { get; set; }
+            [Category("Araba bilgileri"), DisplayName("plaka")]
+            public string Marka { get; set; }
+            [Category("Araba bilgileri"), DisplayName("marka")]
+            public string Model { get; set; }
+            [Category("Araba bilgileri"), DisplayName("model")]
+            public string Yakıt { get; set; }
+            [Category("Araba bilgileri"), DisplayName("yakıt")]
+            public string Renk { get; set; }
+            [Category("Araba görünüş"), DisplayName("renk")]
+            public string Vites { get; set; }
+            [Category("Araba bilgileri"), DisplayName("vites")]
+            public string KasaTipi { get; set; }
+            [Category("Araba görünüş"), DisplayName("kasa tipi")]
+            public string Açıklama { get; set; }
+            [Category("Araba bilgileri"), DisplayName("açıklama")]
+
+            public DateTime Date { get; set; }
+
+        }
+
+        private void DeleteCommand(object sender, EventArgs e)
+        {
+            {
+                if (listView1.SelectedItems.Count == 0)
+                    return;
+                ListViewItem pItem = listView1.SelectedItems[0];
+                Araba secili = pItem.Tag as Araba;
+                var sonuc = MessageBox.Show($"Seçili Araba Silinsin mi?\n\n{secili.Plaka} {secili.Marka}", "Silmeyi onayla", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (sonuc == DialogResult.Yes)
+                {
+                    arabalar.Remove(secili);
+                    listView1.Items.Remove(pItem);
+                }
+
+            }
+
+        }
+
+        private void SaveCommand(object sender, EventArgs e)
+        {
+            SaveFileDialog sf = new SaveFileDialog()
+            {
+                Filter = "Json Formatı|*.json|Xml Formatı|*.xml",
+            };
+            if (sf.ShowDialog() == DialogResult.OK)
+            {
+                if (sf.FileName.EndsWith("json"))
+                {
+                    string data = JsonSerializer.Serialize(arabalar);
+                    File.WriteAllText(sf.FileName, data);
+
+                }
+                else if (sf.FileName.EndsWith("xml"))
+                {
+                    StreamWriter sw = new StreamWriter(sf.FileName);
+                    XmlSerializer serializer = new XmlSerializer(typeof(List<Araba>));
+                    serializer.Serialize(sw, arabalar);
+                    sw.Close();
+                }
+            }
+
+        }
+
+        private void LoadCommand(object sender, EventArgs e)
+        {
+            OpenFileDialog of = new OpenFileDialog()
+            {
+                Filter = "Json, Xml Formatları|*.json;*.xml",
+            };
+            if (of.ShowDialog() == DialogResult.OK)
+            {
+                if (of.ShowDialog() == DialogResult.OK)
+                {
+                    if (of.FileName.ToLower().EndsWith("json"))
+                    {
+                        string jsondata = File.ReadAllText(of.FileName);
+                        arabalar = JsonSerializer.Deserialize<List<Araba>>(jsondata);
+                    }
+                    else if (of.FileName.ToLower().EndsWith("xml"))
+                    {
+                        StreamReader sr = new StreamReader(of.FileName);
+                        XmlSerializer serializer = new XmlSerializer(typeof(List<Araba>));
+                        arabalar = (List<Araba>)serializer.Deserialize(sr);
+                        sr.Close();
+                    }
+                    ShowList();
+
+                }
+            }
+        }
+        string temp = Path.Combine(Application.CommonAppDataPath, "data");
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            string data = JsonSerializer.Serialize(arabalar);
+            File.WriteAllText(temp, data); ;
+
+            base.OnClosing(e);
+        }
+
+        private void hakkındaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new AboutBox1().ShowDialog();
+        }
+    }
+
+    }
+
